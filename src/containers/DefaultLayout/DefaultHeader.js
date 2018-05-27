9309aa32602d807fc858a5ce59d219190613e4a7
Chip Irek <chip.irek@gmail.com>
2018-05-27 18:42:29 -0400

    Removed the hamburger menu(s) that slide out the left nav menu

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -21,12 +21,12 @@ class DefaultHeader extends Component {
 
     return (
       <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        
         <AppNavbarBrand
           full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
           minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
         />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
+
         <Nav className="d-md-down-none" navbar>
           <NavItem className="px-3">
             <NavLink href="/">Dashboard</NavLink>
